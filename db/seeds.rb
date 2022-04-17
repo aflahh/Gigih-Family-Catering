b0a5797014b41292c9efc7340f26e0a2b90a5569
Aflah Hilmy <74708771+aflahh@users.noreply.github.com>
2022-04-17 18:21:06 +0000

    Create Menu & Category model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Category.create(name: 'Makanan')
+Category.create(name: 'Goreng')
+Menu.create(name: 'Nasi Goreng', price: '25.000', description: 'Nasi digoreng')
+Menu.create(name: 'Mie Goreng', price: '20.000', description: 'Mie digoreng')
+MenuCategory.create(menu_id: 1, category_id: 1)
+MenuCategory.create(menu_id: 1, category_id: 2)
+MenuCategory.create(menu_id: 2, category_id: 1)
+MenuCategory.create(menu_id: 2, category_id: 2)
